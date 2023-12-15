b31de2f95e80ccb6d04b80213404d53637d0dcb7
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-12-15 22:35:00 +0800

    Init commit. Added color sets and enums with static members for ui colors and symbols

---
+++ LocalBirds/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  LocalBirds
+//
+//  Created by Rory Allen on 15/12/2023.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ LocalBirds/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  LocalBirds
+//
+//  Created by Rory Allen on 15/12/2023.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
