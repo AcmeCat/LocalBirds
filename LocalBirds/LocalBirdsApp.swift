ad7cd25200f827b2686b4c06ad40275e9018657a
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-12-15 22:16:45 +0800

    Initial Commit

---
+++ LocalBirds/LocalBirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalBirdsApp.swift
+//  LocalBirds
+//
+//  Created by Rory Allen on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
