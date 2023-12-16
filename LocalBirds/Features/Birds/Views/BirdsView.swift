4bc2247a22f4f5f2c8e0830f749ed50c97905bba
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-12-16 14:36:34 +0800

    fleshed out ui for the birds view

--- LocalBirds/Features/Birds/Views/BirdsView.swift
+++ LocalBirds/Features/Birds/Views/BirdsView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct BirdsView: View {
+    private let columns = Array(repeating: GridItem(.flexible()),
+                                count: 2)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Theme.background
+                    .ignoresSafeArea(edges: .top)
+                ScrollView {
+                    LazyVGrid(columns: columns, 
+                              spacing: 16)  {
+                        ForEach(0...5, id: \.self) { item in
+                            VStack(spacing: .zero) {
+                                Rectangle()
+                                    .fill(.blue)
+                                    .frame(height: 130)
+                                VStack {
+                                    Text("#\(item)")
+                                        .font(
+                                            .system(.caption, design: .rounded)
+                                            .bold()
+                                        )
+                                        .foregroundColor(.white)
+                                        .padding(.horizontal, 9)
+                                        .padding(.vertical, 4)
+                                        .background(Theme.pill, in: Capsule())
+                                    Text("<Common Name><Summink>")
+                                        .foregroundColor(Theme.text)
+                                        .font(
+                                            .system(.body, design: .rounded)
+                                        )
+                                }
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,
+                                       alignment: .leading)
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 5)
+                                .background(Theme.detailBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                            .shadow(color: Theme.text.opacity(0.1), radius: 2, x: 0, y: 1)
+                        }
+                    }
+                              .padding()
+                }
+            }
+            .navigationTitle("Birds")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        
+                    } label: {
+                        Symbols.plus
+                            .font(
+                                .system(.headline, design: .rounded)
+                                .bold()
+                            )
+                    }
+                }
+            }
+        }
     }
 }
 
