36ea312358e908d7740a75a056d0484f73d3159b
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2023-12-18 11:11:23 +0800

    ensure update to observed object is handled on main thread (avoid ui update issues)

--- LocalBirds/Features/Birds/View Models/BirdsViewModel.swift
+++ LocalBirds/Features/Birds/View Models/BirdsViewModel.swift
@@ -13,13 +13,14 @@ final class BirdsViewModel: ObservableObject {
     
     func fetchBirds() {
         APINetworkingManager.shared.request("https://nuthatch.lastelm.software/v2/birds", type: AllBirdsResponse.self) { [weak self] res in
-            switch res {
-            case .success(let result):
-                self?.birds = result.entities
-            case .failure(let error):
-                print(error)
+            DispatchQueue.main.async {
+                switch res {
+                case .success(let result):
+                    self?.birds = result.entities
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
-
     }
 }
